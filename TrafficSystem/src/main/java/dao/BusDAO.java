6ca63ee5f70ce1c77b385a23183cbe9c1521de95
Mohamed Soliman <mohamedsolaiman1012@gmail.com>
2025-05-11 17:22:17 +0300

    made classes files

---
+++ TrafficSystem/src/main/java/dao/BusDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.AavengersTrafficControle.trafficsystem.model.*;
+
+/**
+ *
+ * @author mohamedsoliman
+ */
+public class BusDAO extends VehicleDAO {
+    
+}
